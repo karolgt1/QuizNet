094171c7d6cec82c01b7725ca36afacc725b6b38
karolgt1 <karolgt1@wp.pl>
2019-11-07 19:46:53 +0100

    Dodaj pliki projektów.

---
+++ QuizNet/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QuizNet.Models;
+
+namespace QuizNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<Student> students = new List<Student>()
+        {
+            new Student
+             {
+                Id = 1,
+                Index = 12345,
+                FirstName = "Karol",
+                LastName = "Kozicki"
+            },
+            new Student
+             {
+                Id = 2,
+                Index = 6789,
+                FirstName = "Karol2",
+                LastName = "Kozicki2"
+            }
+        };
+    
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+            return View(student);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
