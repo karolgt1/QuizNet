fb1f3d5c682f05198f1cc0bb30595f053fbc540c
karolgt1 <karolgt1@wp.pl>
2019-12-03 22:23:13 +0100

    Added DTO Service to project

--- QuizNet/Controllers/QuestionController.cs
+++ QuizNet/Controllers/QuestionController.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using QuizNet.BusinessLogic.DTOs;
+using QuizNet.BusinessLogic.Interfaces;
 using QuizNet.DataAccess;
 using QuizNet.DataAccess.Models;
+using QuizNet.Models;
+
 
 namespace QuizNet.Controllers
 {
     public class QuestionController : Controller
     {
         private readonly IQuestionsRepository _questionRepository;
+        private readonly IQuizService _quizService;
 
-        public QuestionController(IQuestionsRepository questionRepository)
+        public QuestionController(IQuestionsRepository questionRepository, IQuizService quizService)
         {
             _questionRepository = questionRepository;
+            _quizService = quizService;
         }
 
         public IActionResult GetAll()
@@ -33,24 +40,35 @@ namespace QuizNet.Controllers
 
         public IActionResult Create()
         {
-            var newQuestion = new Question();
+            var newQuestion = new QuestionFormViewModel();
             return View("QuestionForm",newQuestion);
         }
         public IActionResult Edit(int id)
         {
             var questionToEdit = _questionRepository.GetById(id);
+            var questionViewModel = new QuestionFormViewModel(questionToEdit);
             return View("QuestionForm", questionToEdit);
         }
 
         [HttpPost]
-        public IActionResult Save(Question updatedQuestion)
+        public IActionResult Save(QuestionFormViewModel updatedQuestion)
         {
-            if (updatedQuestion.Id != 0)
-                _questionRepository.Update(updatedQuestion);
-            else
-                _questionRepository.Add(updatedQuestion);
+            if (!ModelState.IsValid)
+                return View("QuestionForm", updatedQuestion);
 
-            return RedirectToAction("Get", new { Id = updatedQuestion.Id });
+            var question = updatedQuestion.GetQuestion();
+            if (question.Id != 0)
+                _questionRepository.Update(question);
+            else
+                _questionRepository.Add(question);
+
+            return RedirectToAction("Get", new { Id = question.Id });
+        }
+        public IActionResult GenerateQuiz()
+        {
+            List<QuestionDto> quiz = _quizService.GenerateQuiz();
+            return View("Quiz", quiz);
         }
     }
+
 }
